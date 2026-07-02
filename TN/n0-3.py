aa75a33004bb48f1e026228f6e84b42fd666cfa8
Isen Naiken <isen.naiken@nexomedis.com>
2026-07-02 16:32:37 +0200

    new image processing algo

---
+++ TN/n0-3.py
@@ -0,0 +1,1 @@
+# si pas de ganglion = N0, si ganglion unilatéral = N1, si bilatéral = N2, si uni ou bi mais > 6 cm = N3
---
+++ TN/t1-3.py
@@ -0,0 +1,1 @@
+# si diamètre max de la tumeur ≤ 2 cm = T1, si > 2 cm et ≤ 4 cm = T2, si > 4 cm = T3
